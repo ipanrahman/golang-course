f182d117868a291d4345414818ed29f4d0d285d8
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2021-10-29 21:55:53 +0700

    contoh mock 2

--- unit-test/category_service_test.go
+++ unit-test/category_service_test.go
@@ -12,10 +12,25 @@ var (
 	categoryService    = CategoryService{Repository: categoryRepository}
 )
 
-func TestCategoryService_FindByID(t *testing.T) {
+func TestCategoryService_FindByIDNotFound(t *testing.T) {
 	categoryRepository.mock.On("FindByID", "1").Return(nil)
 
 	category, err := categoryService.FindByID("1")
 	assert.Nil(t, category)
 	assert.NotNil(t, err)
+}
+
+func TestCategoryService_FindByIDFound(t *testing.T) {
+	category := Category{
+		ID:   "1",
+		Name: "Elektronik",
+	}
+	categoryRepository.mock.On("FindByID", "2").Return(category)
+
+	result, err := categoryService.FindByID("2")
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+
+	assert.Equal(t, category.ID, category.ID)
+	assert.Equal(t, category.Name, category.Name)
 }
