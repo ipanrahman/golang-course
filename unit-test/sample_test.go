b6006556e0977467c06e947efab1966065e66061
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2021-10-29 20:48:42 +0700

    menggagalkan unit test dengan fail dan failNow

--- unit-test/sample_test.go
+++ unit-test/sample_test.go
@@ -1,10 +1,22 @@
 package unittest
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Ipan")
 	if result != "Hello, Ipan" {
-		panic("Result is not ipan")
+		t.Fail()
 	}
+	fmt.Println("Testing TestHelloWorld done.")
+}
+
+func TestHelloAhmad(t *testing.T) {
+	result := HelloWorld("Bukan Ahmad")
+	if result != "Hello, Ahmad" {
+		t.FailNow()
+	}
+	fmt.Println("Testing TestHelloAhmad done.")
 }
